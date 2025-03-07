248eba456a56cddb19d7feed4abc1d694c74bd9c
jovicvla <jovicvla@students.zhaw.ch>
2025-03-07 13:08:57 +0100

    Add documentation in code

--- P01/bankAccount.py
+++ P01/bankAccount.py
@@ -1,16 +1,47 @@
 class BankAccount:
+
+    """
+    A class representing a simple bank account with basic operations.
+    
+    This class implements standard bank account functionalities including deposits,
+    withdrawals, balance checks, and account closure. It also enforces constraints
+    such as maximum balance limit and prevents operations on closed accounts.
+    """
     def __init__(self, identifier):
+        """
+        New accounts start with a zero balance
+        Accounts are open by default when created
+        """
         self.identifier = identifier
         self.balance = 0
         self.is_open = True
 
     def deposit(self, amount):
+        """
+        Parameters:
+            amount (float): The amount to deposit
+            
+        Checks:
+            - Account must be open
+            - Amount must be positive
+            - Total balance must not exceed 100K
+        """
         if self.is_open and amount > 0 and self.balance + amount <= 100000:
             self.balance += amount
         else:
             print("Invalid deposit.")
 
     def withdraw(self, amount):
+
+        """
+        Parameters:
+            amount (float): The amount to withdraw
+            
+        Checks:
+            - Account must be open
+            - Amount must be positive
+            - Balance must be sufficient for withdrawal
+        """
         if self.is_open and amount > 0 and self.balance - amount >= 0:
             self.balance -= amount
         else:
@@ -20,6 +51,11 @@ class BankAccount:
         return self.balance
 
     def close_account(self):
+
+        """
+        Checks:
+            - Account balance must be zero
+        """
         if self.balance == 0:
             self.is_open = False
         else:
