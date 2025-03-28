c8bbce6e9a4409355c41e3713a38bb1b6bbe8203
maros-99 <marcorosasco@icloud.com>
2025-03-28 08:18:33 +0000

    docstrings

--- P02/class_files/exchange.py
+++ P02/class_files/exchange.py
@@ -3,23 +3,35 @@ import os
 import json
 
 class ExchangeRates:
-    # The API URL that returns exchange rates with CHF as the base currency
+    """
+    A class to retrieve and store exchange rates with CHF as base currency.
+    It gets exchange rate data from an online API and saves it locally in a JSON file.
+    """
+
     API_URL = "https://open.er-api.com/v6/latest/CHF"
     FILE_NAME = "exchange.json"
 
     def __init__(self):
-        # Load saved rates or get new ones
+        """
+        Initializes ExchangeRates object by loading saved rates from a JSON file if available,
+        if there aren't valid saved rates, it gets new rates via API.
+        """
         self.rates = self.load_or_get_rates()
 
     def load_or_get_rates(self):
-        # Try to load saved exchange rates from a file
+        """
+        Loads saved exchange rates from the local JSON file if they exist and are valid.
+        If not, it gets new exchange rate data from the internet and saves them locally.
+
+        Returns:
+            A dictionary that contains currency codes as keys and their exchange rates as values.
+        """
         if os.path.exists(self.FILE_NAME):
             with open(self.FILE_NAME, "r") as file:
                 data = json.load(file)
                 if data.get("base") == "CHF" and "rates" in data:
                     return data["rates"]
 
-        # If not found or invalid, get rates from the internet
         response = requests.get(self.API_URL)
         if response.status_code == 200:
             data = response.json()
@@ -31,19 +43,36 @@ class ExchangeRates:
             return {}
 
     def fetch_rates(self):
-        # Make a request to the API to get the exchange rate data
+        """
+        Retrieves the latest exchange rates from the online API.
+
+        Returns:
+            Dictionary
+
+        Raises:
+            Exception: If the API request fails (200 status code).
+        """
         response = requests.get(self.API_URL)
 
-        # If the request was successful (HTTP status 200)
         if response.status_code == 200:
             data = response.json()
-            # Extract the 'rates' dictionary from the JSON response
             return data.get("rates", {})
         else:
-            # If something went wrong, raise an error
             raise Exception("API request failed with status code " + str(response.status_code))
 
     def get_rate_to_chf(self, currency):
+        """
+        Retrieves the exchange rate of a specific currency to CHF.
+
+        Arguments:
+            currency (str): Currency code to get the exchange rate.
+
+        Returns:
+            amount (float): The exchange rate of the specified currency to CHF.
+
+        Raises:
+            ValueError: If the specified currency is not found.
+        """
         currency = currency.upper()
         rate = self.rates.get(currency)
         if rate:
@@ -51,5 +80,15 @@ class ExchangeRates:
         raise ValueError("Currency '" + currency + "' not found.")
 
     def convert_to_chf(self, amount, currency):
+        """
+        Converts a specified amount of a currency to value in CHF.
+
+        Args:
+            amount (float): The amount of money to convert.
+            currency (str): The currency code of the money to be converted.
+
+        Returns:
+            float: The equivalent amount in CHF.
+        """
         rate = self.get_rate_to_chf(currency)
         return amount / rate
